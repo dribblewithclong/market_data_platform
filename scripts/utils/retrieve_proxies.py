d640fc9e7398c7ba4e45df31376d56953bc27e9f
nguyen cao long <longnc@yes4all.com>
2024-09-13 14:13:13 +0700

    change proxy func

--- scripts/utils/retrieve_proxies.py
+++ scripts/utils/retrieve_proxies.py
@@ -1,6 +1,7 @@
 import random
 import os
 import pandas as pd
+from collections import deque
 
 
 def load_proxies(proxies_path: str) -> list:
@@ -35,6 +36,47 @@ def generate_proxy_html(
         }
 
     return proxy_html
+
+
+class ProxiesPool(deque):
+    def __init__(
+        self,
+        proxies_path: str,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.proxies_path = proxies_path
+        self._generate_pool()
+
+    @staticmethod
+    def load_proxies(
+        proxies_path: str,
+    ) -> list:
+        if not proxies_path:
+            return None
+        if isinstance(
+            proxies_path,
+            str
+        ):
+            proxies = pd.read_pickle(proxies_path)
+            proxies_list = [
+                f'{x["proxy_address"]}:{x["port"]}'
+                for x in proxies
+            ]
+            random.shuffle(proxies_list)
+            return proxies_list
+        else:
+            raise TypeError(
+                f"Proxies path must be str. "
+                f"Receive {type(proxies_path)}"
+            )
+
+    def _generate_pool(self):
+        proxies = self.load_proxies(self.proxies_path)
+        if proxies:
+            for proxy in proxies:
+                self.append(proxy)
 
 
 if __name__ == '__main__':
